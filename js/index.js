8fdfb3bf131565792eb34c440e969a7f16d8d2fe
kedi <kedi@KDs-Macbook-Air-2.local>
2023-04-18 17:57:50 -0400

    fixed index.js

--- js/index.js
+++ js/index.js
@@ -7,14 +7,13 @@ function menuDrop() {
 
 // Get the current URL path
 var url = window.location.pathname;
-
 // Get all the menu items in the navigation bar
 var menuItems = document.querySelectorAll('.header_nav_pc a');
 
 // Loop through each menu item and check if its href matches the current URL path
 
 menuItems.forEach(function(menuItem) {
-  if ("/" + menuItem.getAttribute('href') === url) {
+  if (menuItem.getAttribute('href') === url) {
     console.log(menuItem);
     // If the href matches, add the active class to the menu item
     menuItem.classList.add('active');
@@ -25,7 +24,7 @@ menuItems.forEach(function(menuItem) {
 
 var menuItemsMobile = document.querySelectorAll('#dropDownContent a'); 
 menuItemsMobile.forEach(function(menuItem) {
-  if ("/" + menuItem.getAttribute('href') === url) {
+  if (menuItem.getAttribute('href') === url) {
     console.log(menuItem);
     // If the href matches, add the active class to the menu item
     menuItem.classList.add('active_mobile');
