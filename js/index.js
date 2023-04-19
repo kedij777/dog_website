e831bbc6f36c0fadde375a4aea3f401d07d0e999
kedij777 <98619414+kedij777@users.noreply.github.com>
2023-04-18 20:33:24 -0400

    fix root url's menu bar

--- js/index.js
+++ js/index.js
@@ -7,17 +7,21 @@ function menuDrop() {
 
 // Get the current URL path
 var url = window.location.href;
+var rootUrl = ''https://kedij777.github.io/dog_website/';
 // Get all the menu items in the navigation bar
 var menuItems = document.querySelectorAll('.header_nav_pc a');
 
+// At root, assign it to home
+if (url == rootUrl){
+      menuItems[0].classList.add('active');
+}
 // Loop through each menu item and check if its href matches the current URL path
-
 menuItems.forEach(function(menuItem) {
   if (menuItem.href === url) {
     console.log(menuItem);
     // If the href matches, add the active class to the menu item
     menuItem.classList.add('active');
-  }
+  }  
 });
 
 // do the same thing for mobile mode
